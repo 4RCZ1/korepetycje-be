6f96a2f94be57ed1fe99d1ceec2a55314b322a0c
Dominik Bałaban <dombal4@st.amu.edu.pl>
2025-05-28 18:13:32 +0200

    clean up the database entities

--- src/HelloWorld/Student.cs
+++ src/HelloWorld/Student.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace HelloWorld;
 
@@ -10,12 +6,15 @@ public class Student
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
     public string Name { get; set; }
+
     [Required]
     public string Surname { get; set; }
+
     [Required]
     public string Address { get; set; }
-    
+
     public List<Serie>? Series { get; set; }
 }
