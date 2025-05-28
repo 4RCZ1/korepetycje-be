9dad09e8d000911fcf6adecf05ecb3f672520a92
Dominik Bałaban <dombal4@st.amu.edu.pl>
2025-05-28 20:58:21 +0200

    re-migrate the database

---
+++ src/HelloWorld/Migrations/20250528185750_InitialMigration.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HelloWorld.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "student",
+                columns: table => new
+                {
+                    student_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    address = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_student", x => x.student_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "schedule",
+                columns: table => new
+                {
+                    schedule_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    student_id = table.Column<int>(type: "integer", nullable: false),
+                    begin_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    period = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    end_ordinal = table.Column<int>(type: "integer", nullable: false),
+                    lesson_duration = table.Column<TimeSpan>(type: "interval", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_schedule", x => x.schedule_id);
+                    table.ForeignKey(
+                        name: "FK_schedule_student_student_id",
+                        column: x => x.student_id,
+                        principalTable: "student",
+                        principalColumn: "student_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "lesson",
+                columns: table => new
+                {
+                    schedule_id = table.Column<int>(type: "integer", nullable: false),
+                    ordinal = table.Column<int>(type: "integer", nullable: false),
+                    custom_duration = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    tutor_info = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    is_confirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    has_occurred = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lesson", x => new { x.schedule_id, x.ordinal });
+                    table.ForeignKey(
+                        name: "FK_lesson_schedule_schedule_id",
+                        column: x => x.schedule_id,
+                        principalTable: "schedule",
+                        principalColumn: "schedule_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_schedule_student_id",
+                table: "schedule",
+                column: "student_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "lesson");
+
+            migrationBuilder.DropTable(
+                name: "schedule");
+
+            migrationBuilder.DropTable(
+                name: "student");
+        }
+    }
+}
