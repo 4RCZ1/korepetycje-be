d52592d138e37cff7205d6600e4bc3bf676ef6d6
Dominik Bałaban <dombal4@st.amu.edu.pl>
2025-05-31 17:36:25 +0200

    INZ-26 re-migrate the database after directory cleanup

--- src/Database/Migrations/20250531161647_Initial.cs
+++ src/Database/Migrations/20250531161647_Initial.cs
@@ -4,10 +4,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace HelloWorld.Migrations
+namespace Database.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialMigration : Migration
+    public partial class Initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
--- src/Database/OurDbContext.cs
+++ src/Database/OurDbContext.cs
@@ -6,9 +6,13 @@ namespace Database;
 
 public class OurDbContext : DbContext
 {
-    public OurDbContext(DbContextOptions<OurDbContext> options)
-        : base(options) { }
     public DbSet<DbLesson> Lessons { get; set; }
     public DbSet<DbStudent> Students { get; set; }
     public DbSet<DbSchedule> Schedules { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql();
+    }
 }
