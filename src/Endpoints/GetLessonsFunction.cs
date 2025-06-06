9c25d0b7310cd1d81e5fceeb8a6165cca906de59
Dominik Bałaban <dombal4@st.amu.edu.pl>
2025-06-06 11:52:32 +0200

    merge GetStudentLessonsFunction and GetLessonsFunction for unambiguous template paths

--- src/Endpoints/GetLessonsFunction.cs
+++ src/Endpoints/GetLessonsFunction.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using Endpoints.Interfaces;
 
 [assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
@@ -13,10 +14,22 @@ public class GetLessonsFunction
         APIGatewayProxyRequest request, ILambdaContext context)
     {
         var service = await ServiceFactory.CreateTimetableService();
-        var lessons = service.GetLessons(
-            request.QueryStringParameters["start-date"],
-            request.QueryStringParameters["end-date"]);
+        var studentExternalId = request.QueryStringParameters["student-external-id"];
+        if (studentExternalId == null)
+        {
+            return OkJson(service.GetLessons(
+                request.QueryStringParameters["start-date"],
+                request.QueryStringParameters["end-date"]));
+        }
+        else
+        {
+            // todo: handle date ranges
+            return OkJson(service.GetStudentLessons(studentExternalId));
+        }
+    }
 
+    private static APIGatewayProxyResponse OkJson(IList<LessonDto> lessons)
+    {
         return new APIGatewayProxyResponse
         {
             Body = JsonSerializer.Serialize(lessons),
