9153b71277f98ceafe18977342da211464c73932
Dominik Bałaban <dombal4@st.amu.edu.pl>
2025-06-13 19:39:12 +0200

    INZ-46 connect to Postgres instance specified by KOREPETYCJE_POSTGRES

--- src/Endpoints/ServiceFactory.cs
+++ src/Endpoints/ServiceFactory.cs
@@ -24,6 +24,14 @@ internal static class ServiceFactory
 
     private static async Task<string> GetConnectionStringAsync()
     {
+        var environmentConnection = Environment.GetEnvironmentVariable("KOREPETYCJE_POSTGRES");
+        if (!string.IsNullOrWhiteSpace(environmentConnection))
+            return environmentConnection;
+        return await GetCloudConnectionStringAsync();
+    }
+
+    private static async Task<string> GetCloudConnectionStringAsync()
+    {
         var usernamePassword = await GetSecretAsync("rds!db-038b4d1d-cb2e-4fa9-95f9-50e04af2e276");
         var hostPort = await GetSecretAsync("dev/dbhost");
         return $"Server={hostPort["endpoint"]};Database=postgres;Port={hostPort["port"]};" +
