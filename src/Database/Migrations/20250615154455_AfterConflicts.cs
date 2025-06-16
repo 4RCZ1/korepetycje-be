ee824d4cf049386556dfeb0e15cea786dd6cff9f
KatMularczyk <k.mularczyk22@gmail.com>
2025-06-16 22:08:05 +0200

    Forgotten migrations commit

---
+++ src/Database/Migrations/20250615154455_AfterConflicts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AfterConflicts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "period",
+                table: "schedule",
+                type: "interval",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "period",
+                table: "schedule",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval",
+                oldNullable: true);
+        }
+    }
+}
