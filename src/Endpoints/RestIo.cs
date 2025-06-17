aff54786d50fb0289a7d23395bcb66164420a94d
4RCZ1 <marcinowicz1@gmail.com>
2025-06-17 23:09:29 +0200

    INZ-62 add CORS headers to API responses

--- src/Endpoints/RestIo.cs
+++ src/Endpoints/RestIo.cs
@@ -10,6 +10,13 @@ namespace Endpoints;
 
 public static class RestIo
 {
+    private static readonly Dictionary<string, string> CorsHeaders = new()
+    {
+        { "Access-Control-Allow-Origin", "*" },
+        { "Access-Control-Allow-Headers", "Content-Type,Authorization" },
+        { "Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE,PATCH" }
+    };
+    
     public static async Task<APIGatewayProxyResponse> HandleRestExceptionsAsync<T>(Func<Task<T>> f)
     {
         try
@@ -19,14 +26,15 @@ public static class RestIo
         catch (BadRequestException e)
         {
             Console.WriteLine(e);
+            var headers = new Dictionary<string, string>(CorsHeaders)
+            {
+                { "Content-Type", "text/plain" }
+            };
             return new APIGatewayProxyResponse
             {
                 Body = e.Message,
                 StatusCode = 400,
-                Headers = new Dictionary<string, string>
-                {
-                    { "Content-Type", "text/plain" },
-                },
+                Headers = headers,
             };
         }
     }
@@ -64,14 +72,16 @@ public static class RestIo
 
     private static APIGatewayProxyResponse OkJson(Object objectToSerialize)
     {
+        var headers = new Dictionary<string, string>(CorsHeaders)
+        {
+            { "Content-Type", "application/json" }
+        };
+        
         return new APIGatewayProxyResponse
         {
             Body = JsonSerializer.Serialize(objectToSerialize, JsonOptions),
             StatusCode = 200,
-            Headers = new Dictionary<string, string>
-            {
-                { "Content-Type", "application/json" },
-            },
+            Headers = headers,
         };
     }
 
