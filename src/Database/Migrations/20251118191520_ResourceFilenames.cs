2024d29b389b83f181f8db880fc700232c1fc2a7
Dominik Bałaban <dombal4@st.amu.edu.pl>
2025-11-18 20:15:58 +0100

    INZ-123 Migrate the database for resource filenames

---
+++ src/Database/Migrations/20251118191520_ResourceFilenames.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResourceFilenames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "file_path",
+                table: "resource",
+                newName: "file_name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "file_name",
+                table: "resource",
+                newName: "file_path");
+        }
+    }
+}
