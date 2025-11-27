4274d62f93ea9ca6ab0b8bf70a5f40177ae052bf
Dominik Bałaban <dombal4@st.amu.edu.pl>
2025-11-27 22:27:04 +0100

    INZ-124 Add an ID field to ResourceGroupDto

--- src/Endpoints.Dto/ResourceGroupDto.cs
+++ src/Endpoints.Dto/ResourceGroupDto.cs
@@ -2,6 +2,7 @@ namespace Endpoints.Dto;
 
 public class ResourceGroupDto
 {
+    public string? Id { get; set; }
     public required string Name { get; set; }
     public required IList<ResourceDto> Resources { get; set; }
 }
