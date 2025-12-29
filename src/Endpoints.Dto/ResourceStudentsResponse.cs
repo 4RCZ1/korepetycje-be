e34bdfc293f740ff83d1912e0a8362d91eb5f22c
KatMularczyk <k.mularczyk22@gmail.com>
2025-12-29 01:31:30 +0100

    create resource uberendpoint

---
+++ src/Endpoints.Dto/ResourceStudentsResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace Endpoints.Dto;
+
+public class ResourceStudentsResponse
+{
+    public class ResourceWithStudentsResponse
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public List<StudentDto> DirectStudents { get; set; } = new();
+        public List<StudentGroupWithStudentsDto> StudentGroups { get; set; } = new();
+        public List<ResourceGroupWithStudentsDto> ResourceGroups { get; set; } = new();
+    }
+
+    public class StudentGroupWithStudentsDto
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public bool IsSingle { get; set; }
+        public List<StudentDto> Students { get; set; } = new();
+    }
+
+    public class ResourceGroupWithStudentsDto
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public bool IsSingle { get; set; }
+        public List<StudentDto> DirectStudents { get; set; } = new();
+        public List<StudentGroupWithStudentsDto> StudentGroups { get; set; } = new();
+    }
+}
---
+++ src/Services.Interfaces/IResourceStudentsDao.cs
@@ -0,0 +1,15 @@
+﻿using Database.Entities;
+
+namespace Services.Interfaces;
+
+public interface IResourceStudentsDao
+{
+    ResourceStudentStructure GetResourceStudentStructure(int resourceId);
+}
+
+public class ResourceStudentStructure
+{
+    public List<DbStudent> DirectStudents { get; set; } = new();
+    public List<DbStudentGroup> StudentGroups { get; set; } = new();
+    public List<DbResourceGroup> ResourceGroups { get; set; } = new();
+}
